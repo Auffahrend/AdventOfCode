dca99ce91f85303c0902dec8a14d3a3851375f35
Andrii Kostenko <akkosten@amazon.com>
2023-12-07 22:39:29 -0800

    Day 08/2

--- src/main/rust/src/advent2023/day08.rs
+++ src/main/rust/src/advent2023/day08.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::string::ToString;
-
+use num_integer::lcm;
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -13,7 +13,18 @@ pub(crate) fn solve() {
 fn solution(input: &String) -> i64 {
     let navig = Navigation::parse(input);
 
-    navig.find_end()
+    // Part 1
+    // navig.find_end()
+
+    // part 2
+    let steps_per_start: Vec<i64> = navig.nodes.keys()
+        .filter(|&n| n.chars().nth(2) == Some('A'))
+        .map(|start| navig.steps_till_end(start))
+        .collect();
+
+    println!("Found following steps for each start: {:?}", steps_per_start);
+
+    steps_per_start.iter().fold(1i64, |acc, &x| lcm(acc, x))
 }
 
 
@@ -25,7 +36,7 @@ struct Navigation {
 const START: &str = "AAA";
 const END: &str = "ZZZ";
 lazy_static! {
-    static ref NODE_RE : Regex = Regex::new(r"([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)").unwrap();
+    static ref NODE_RE : Regex = Regex::new(r"([A-Z0-9]+) = \(([A-Z0-9]+), ([A-Z0-9]+)\)").unwrap();
 }
 
 impl Navigation {
@@ -57,6 +68,24 @@ impl Navigation {
 
         return steps
     }
+
+    fn steps_till_end(&self, start: &str) -> i64 {
+        let mut steps = 0i64;
+        let mut current: &str = start;
+
+        while current.chars().nth(2) != Some('Z') {
+            let turn = self.instructions[steps as usize % self.instructions.len()];
+            steps += 1;
+            current = if turn == 'L' {
+                &(self.nodes.get(current).unwrap().0)
+            } else {
+                &(self.nodes.get(current).unwrap().1)
+            };
+            println!("Moved to {}", current)
+        }
+
+        return steps
+    }
 }
 
 const TEST_1: TestVals<&str, i64> = TestVals(&"RL
@@ -69,10 +98,15 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)
 ", 2i64);
-const TEST_2: TestVals<&str, i64> = TestVals(&"LLR
+const TEST_2: TestVals<&str, i64> = TestVals(&"LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
 ", 6i64);
 const FILE: &str = "../resources/advent2023/day08.txt";
